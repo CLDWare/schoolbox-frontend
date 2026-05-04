a86e50b794372b958faf1e1aeb428695e9a28a19
kittendevv <codingkitten@disroot.org>
2026-05-04 22:01:51 +0200

    Verwijder sveltekit rewrite folder code op losse branch

--- svelte-rewrite/src/routes/+layout.server.ts
+++
@@ -1,7 +0,0 @@
-import type { LayoutServerLoad } from './$types';
-
-export const load = (async ({ fetch }) => {
-	const res = await fetch('/api/me');
-	const me = await res.json();
-	return { me };
-}) satisfies LayoutServerLoad;
--- svelte-rewrite/src/routes/session/+page.server.ts
+++
@@ -1,9 +0,0 @@
-import type { PageServerLoad } from './$types';
-import { PUBLIC_URL } from '$env/static/public';
-import type { ApiResponse, Session } from '$lib/types';
-
-export const load: PageServerLoad = async ({ fetch }) => {
-	const res = await fetch('/api/session/current');
-	const session: ApiResponse<Session> = await res.json();
-	return { session };
-};
